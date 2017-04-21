67b6cce21e9390db9bf4d424f99afe861e5d4f0c
kwadrnt <kevinctse@gmail.com>
2017-04-20 20:46:44 -0700

    attempt #2

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/sneakerhub" );
 var mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost/sneakerhub");
 
 module.exports.Shoe = require('./shoe');
 module.exports.Brand = require('./brand');
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/sneakerhub" );
+// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/sneakerhub" );
