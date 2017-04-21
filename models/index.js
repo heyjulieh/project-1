a76c940292fa126fda82d692a932c6746372794a
kwadrnt <kevinctse@gmail.com>
2017-04-20 20:57:36 -0700

    fixing connection for heroku

--- models/index.js
+++ models/index.js
@@ -1,8 +1,7 @@
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/sneakerhub" );
+
 var mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost/sneakerhub");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/sneakerhub" );
 
 module.exports.Shoe = require('./shoe');
 module.exports.Brand = require('./brand');
 
-// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/sneakerhub" );
